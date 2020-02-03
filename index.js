f033b2a11aab03b6c19356c0bf4faaa90ca263d5
Suchron Rizal <suchronrizal@gmail.com>
2020-02-03 19:30:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/myfirst_mongodb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("connect");
+  })
+  .catch((err) => console.error("error connection", err));
+
+const mongooseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: new Date() },
+  isPublish: Boolean
+});
+
+//save data
+const Courses = mongoose.model("courses", mongooseSchema);
+async function createCourses() {
+  const course = new Courses({
+    name: "Nur Cholifah",
+    author: "choifah",
+    tags: ["multimedia", "html", "css3", "javascript"],
+    isPublish: true
+  });
+  const result = await course.save();
+  console.log(result);
+}
+
+//pagination & filtering
+async function getCourses() {
+  const pageNumber = 2;
+  const pageSize = 10;
+  // /api/courses?pageNumber=3&pageSize=10
+  const find = await Courses.find({ name: "Rizal" })
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    .select({ name: 1 });
+  console.log(find);
+}
+
+//updat data
+async function updateCourses(id) {
+  const course = await Courses.findById(id);
+  if (!course) return;
+
+  course.isPublish = true;
+  course.author = "Another author";
+
+  const result = await course.save();
+  console.log(result);
+}
+
+//update a document
+async function updateDocument(id) {
+  const result = await Courses.update(
+    { _id: id },
+    {
+      $set: {
+        name: "Aldi",
+        isPublish: false
+      }
+    }
+  );
+  console.log(result);
+}
+
+updateDocument("5e37a8ce09b0c7032cd0f0fa");
